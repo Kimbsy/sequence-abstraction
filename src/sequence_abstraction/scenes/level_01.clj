79817406a7bd733be783ce02864ae8443f8a1c47
Kimbsy <lordkimber@gmail.com>
2021-10-20 20:44:24 +0100

    Remove buffer, allow aminos to draw their own pairs
    This has massively simplified the whole setup, now each amino has two
    images and knows to display it;s right half when it's `:paired?` is
    true.
    
    Adding some simple on-screen controls to aid testing.

--- src/sequence_abstraction/scenes/level_01.clj
+++ src/sequence_abstraction/scenes/level_01.clj
@@ -7,7 +7,6 @@
             [sequence-abstraction.common :as common]
             [sequence-abstraction.sprites.amino :as amino]
             [sequence-abstraction.sprites.border :as border]
-            [sequence-abstraction.sprites.buffer :as buffer]
             [sequence-abstraction.sprites.countdown :as countdown]
             [sequence-abstraction.sprites.dna :as dna]
             [sequence-abstraction.sprites.fade :as fade]))
@@ -17,6 +16,8 @@
    :buffer
    :border
    :fade
+   :control-images
+   :control-text
    :inserter
    :score
    :combo
@@ -38,18 +39,7 @@
                  (fn [aminos]
                    (map (fn [a]
                           (-> a
-                              (assoc :vel [0 0])
-                              (update :pair
-                                      (fn [p]
-                                        (when p
-                                          (assoc p :vel [0 0])))))
-
-                          #_(qptween/add-tween
-                           a
-                           (qptween/->tween :vel
-                                            (- amino/amino-speed)
-                                            :update-fn qptween/tween-y-fn
-                                            :step-count 2)))
+                              (assoc :vel [0 0])))
                         aminos)))))))
 
 (defn check-stop
@@ -59,7 +49,7 @@
                       (filter #(= :dna (:sprite-group %)))
                       first
                       :aminos
-                      (filter #(nil? (:pair %))))]
+                      (remove :paired?))]
       (if (some #(< (* 0.6 (q/height)) (second (:pos %))) unpaired)
         (stop-aminos state)
         state))
@@ -89,7 +79,16 @@
    (border/->border [(+ 84 (* 0.5 (q/width))) 375] (q/height) common/cultured)
 
    (dna/add-more-aminos (dna/->dna))
-   (buffer/->buffer)])
+
+   (qpsprite/image-sprite :control-images [620 320] 92 21 "img/green-right.png")
+   (qpsprite/image-sprite :control-images [620 350] 92 21 "img/red-right.png")
+   (qpsprite/image-sprite :control-images [620 380] 92 21 "img/purple-right.png")
+   (qpsprite/image-sprite :control-images [620 410] 92 21 "img/turquoise-right.png")
+
+   (qpsprite/text-sprite "C" [550 327] :color common/cultured)
+   (qpsprite/text-sprite "A" [550 357] :color common/cultured)
+   (qpsprite/text-sprite "T" [550 387] :color common/cultured)
+   (qpsprite/text-sprite "G" [550 417] :color common/cultured)])
 
 (defn remove-amino-from-buffer
   [{:keys [current-scene] :as state}]
@@ -99,38 +98,32 @@
    (fn [buffer]
      (update buffer :aminos pop))))
 
-(defn attach-right-amino
-  "Remove the next amino from the buffer and pair it to the next
-  unpaired amino in the dna."
-  [{:keys [current-scene] :as state}]
-  (let [amino (buffer/next-amino (->> (get-in state [:scenes current-scene :sprites])
-                                      (filter (common/group-pred :buffer))
-                                      first))]
-    (-> state
-        (assoc :halted? false)
-        remove-amino-from-buffer
-        (common/update-sprites-by-pred
-         (common/group-pred :dna)
-         (fn [dna]
-           (update dna :aminos
-                   (fn [aminos]
-                     (into clojure.lang.PersistentQueue/EMPTY
-                           (map #(assoc % :vel [0 amino/amino-speed]))
-                           (concat (filter :pair aminos)
-                                   (let [removed (remove :pair aminos)]
-                                     (cons (assoc (first removed) :pair amino)
-                                           (rest removed))))))))))))
-
 (defn handle-amino-input
-  "Add a right-hand amino to the input queue"
-  [state e]
+  "Attempt to add a right-hand amino to the next unpaired left-hand
+  amino"
+  [{:keys [current-scene] :as state} e]
   (let [k (get e :key)]
     (if (#{:c :a :t :g} k)
-      (common/update-sprites-by-pred
-       state
-       (common/group-pred :buffer)
-       (fn [buffer]
-         (buffer/add-amino buffer k)))
+      (let [{:keys [aminos]} (->> (get-in state [:scenes current-scene :sprites])
+                                  (filter (common/group-pred :dna))
+                                  first)
+            removed   (remove :paired? aminos)
+            a         (first removed)
+            correct-k (= (amino/kw->color k) (:pair-color a))
+            updated-a (if correct-k (assoc a :paired? true) a)]
+        (if correct-k
+          (-> state
+              (assoc :halted? false)
+              (common/update-sprites-by-pred
+               (common/group-pred :dna)
+               (fn [dna]
+                 (assoc dna :aminos
+                        (into clojure.lang.PersistentQueue/EMPTY
+                              (map #(assoc % :vel [0 amino/amino-speed]))
+                              (concat (filter :paired? aminos)
+                                      (cons updated-a
+                                            (rest removed))))))))
+          state))
       state)))
 
 (defn init
@@ -139,9 +132,5 @@
    :draw-fn draw-level-01
    :update-fn update-level-01
    :key-pressed-fns [handle-amino-input
-                     (fn [state e]
-                       (if (= :space (:key e))
-                         (attach-right-amino state)
-                         state))
                      (fn [state e] (inc-remaining state 3))]
    :mouse-pressed-fns [(fn [state e] (prn (:y e) (* 0.6 (q/height))) state)]})
--- src/sequence_abstraction/sprites/amino.clj
+++ src/sequence_abstraction/sprites/amino.clj
@@ -1,54 +1,61 @@
 (ns sequence-abstraction.sprites.amino
-  (:require [quip.sprite :as qpsprite]))
+  (:require [quil.core :as q]
+            [quip.sprite :as qpsprite]))
 
 (def amino-speed 2)
+
+(def char->color
+  {\C :green
+   \A :red
+   \T :purple
+   \G :turquoise})
+
+(def kw->color
+  {:c :green
+   :a :red
+   :t :purple
+   :g :turquoise})
+
+(def color->pair-color
+  {:green :turquoise
+   :turquoise :green
+   :red :purple
+   :purple :red})
 
 (defn image-file
   [color side]
   (str "img/" (name color) "-" (name side) ".png"))
 
 (defn update-amino
-  [{:keys [pair] :as amino}]
-  (let [{new-pos :pos :as updated-amino} (qpsprite/update-image-sprite amino)
-        updated-pair (assoc pair :pos new-pos)]
-    (assoc updated-amino updated-pair))
-  #_(if pair
-    (-> amino
-        qpsprite/update-image-sprite
-        (update :pair qpsprite/update-image-sprite))
-    (qpsprite/update-image-sprite amino)))
+  [amino]
+  amino)
 
 (defn draw-amino
-  [{:keys [pair] :as amino}]
-  (qpsprite/draw-image-sprite amino)
-  (when pair
-    (qpsprite/draw-image-sprite pair)))
+  [{:keys [pos w h image pair-image paired?]}]
+  (let [[x y] (qpsprite/offset-pos pos w h)]
+    (q/image image x y)
+    (when paired?
+      (q/image pair-image (+ x 80) y))))
 
 (defn ->amino
   [pos color side & {:keys [vel] :or {vel [0 amino-speed]}}]
-  (-> (qpsprite/image-sprite
-       :amino
-       pos
-       92 21
-       (image-file color side)
-       :vel vel)
-      (assoc :update-fn update-amino)
-      (assoc :draw-fn draw-amino)))
-
-(def color-map
-  {\C :green
-   \A :red
-   \T :purple
-   \G :turquoise
-   :c :green
-   :a :red
-   :t :purple
-   :g :turquoise})
+  (let [left-image-file  (image-file color :left)
+        right-image-file (image-file (color->pair-color color) :right)]
+    {:sprite-group :amino
+     :uuid         (java.util.UUID/randomUUID)
+     :pos          pos
+     :vel          vel
+     :w            92
+     :h            21
+     :update-fn    qpsprite/update-image-sprite
+     :draw-fn      draw-amino
+     :image        (q/load-image left-image-file)
+     :pair-image   (q/load-image right-image-file)
+     :pair-color   (color->pair-color color)
+     :paired?      false}))
 
 (defn new-amino
   [c direction last-pos & {:keys [d-pos vel]
                            :or {d-pos [0 -30]
                                 vel [0 amino-speed]}}]
-  (if-not (get color-map c)
-    (throw (Exception. (str"UNKNOWN amino acid: " c)))
-    (->amino (map + last-pos d-pos) (get color-map c) direction :vel vel)))
+  (->amino (map + last-pos d-pos) (char->color c) direction :vel vel))
